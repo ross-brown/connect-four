696652ede460dee98e66e4b7c0e0167be85c5542
ross-brown <rbrown_98@yahoo.com>
2023-08-09 14:31:12 -0500

    refactor makeBoard to update board variable rather than push to it

--- connect4.js
+++ connect4.js
@@ -11,7 +11,7 @@ const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-const board = []; // array of rows, each row is array of cells  (board[y][x])
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -19,13 +19,15 @@ const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  const emptyBoard = [];
   for (let y = 0; y < HEIGHT; y++) {
     const row = [];
     for (let x = 0; x < WIDTH; x++) {
       row.push(null);
     }
-    board.push(row);
+    emptyBoard.push(row);
   }
+  board = emptyBoard;
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
