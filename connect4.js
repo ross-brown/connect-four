d42ccb4287a29fbae44e6c6f7d701bbc3cefeca2
ross-brown <rbrown_98@yahoo.com>
2023-08-09 16:47:02 -0500

    re-refactor back to pushing rows to global board

--- connect4.js
+++ connect4.js
@@ -11,7 +11,7 @@ const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
+const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -19,15 +19,14 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-  const emptyBoard = [];
   for (let y = 0; y < HEIGHT; y++) {
     const row = [];
     for (let x = 0; x < WIDTH; x++) {
       row.push(null);
     }
-    emptyBoard.push(row);
-  }
-  board = emptyBoard;
+    board.push(row);
+  } 
+  console.log(board);
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
