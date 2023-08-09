0774cc28172caebb355e5983d4a33d4d0ad66ca9
ross-brown <rbrown_98@yahoo.com>
2023-08-09 18:55:05 -0500

    implement handleClick func and endGame func

--- connect4.js
+++ connect4.js
@@ -80,7 +80,7 @@ function placeInTable(y, x) {
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -95,7 +95,8 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
+
   placeInTable(y, x);
 
   // check for win
@@ -104,10 +105,13 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+  if (board.every(row => !row.includes(null))) {
+    endGame("TIE GAME!!!");
+  }
+
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer = (currPlayer === 1 ? 2 : 1);
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
